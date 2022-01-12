01e44b5663cb4ebb3759f0520fe0dd09b3379abd
Mark Tripney <marktripney@icloud.com>
2022-01-12 18:50:05 +0100

    Add Card component.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,21 @@
 <script>
+  import Card from './components/Card.svelte';
   import Footer from './components/Footer.svelte';
+
+  // Card details
+  const basicMonthly = {
+    subLevel: 'Basic',
+    price: 19.99,
+    storage: 500,
+    users: 2,
+    sendLimit: 3,
+  };
 </script>
 
-<main class="site-content" />
+<main class="site-content">
+  <Card {...basicMonthly} />
+</main>
 <Footer />
+
+<style>
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,70 @@
+<script>
+  export let subLevel;
+  export let price;
+  export let users;
+  export let storage;
+  export let sendLimit;
+</script>
+
+<div class="card">
+  <h2>{subLevel}</h2>
+  <ul>
+    <li class="price"><span class="dollar">$</span>{price}</li>
+    <li class="spec">{storage} GB Storage</li>
+    <li class="spec">{users} Users Allowed</li>
+    <li class="spec">Send up to {sendLimit} GB</li>
+  </ul>
+  <button>Learn more</button>
+</div>
+
+<style>
+  .card {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    padding-top: 2rem;
+    background-color: #fff;
+    border-radius: 10px;
+    max-width: 350px;
+    color: var(--gray-blue);
+    padding-inline: 29px;
+  }
+  .dollar {
+    font-size: 2.5rem;
+    margin-right: 0.5rem;
+  }
+  .price {
+    display: flex;
+    font-size: 4.5rem;
+    align-items: center;
+    justify-content: center;
+    border-top: none;
+    color: var(--dark-gray-blue);
+  }
+  ul {
+    width: 100%;
+    list-style: none;
+    text-align: center;
+  }
+  li {
+    text-decoration: none;
+    border-top: 1px solid var(--gray-blue);
+    padding-block: 13px;
+  }
+  li:last-child {
+    border-bottom: 1px solid var(--gray-blue);
+  }
+  button {
+    text-transform: uppercase;
+    width: 100%;
+    background: linear-gradient(#a2a7f0, #696edd);
+    color: #fff;
+    font-size: 0.8125rem;
+    letter-spacing: 1.39px;
+    padding-block: 0.875rem;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    margin-block: 2rem;
+  }
+</style>
